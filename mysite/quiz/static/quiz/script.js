c09d7258b03113e03f7582e44fa6cfa80a7d8b5f
George Baker <gxb911@cs.bham.ac.uk>
2021-11-03 16:56:20 +0000

    Add progress bar which updates from 0 to 100

--- mysite/quiz/static/quiz/script.js
+++ mysite/quiz/static/quiz/script.js
@@ -1,1 +1,15 @@
-document.getElementById("nameInput").focus()
+function updateProgress(progressBarElement, progressBarMessageElement, progress) {
+  progressBarElement.style.width = progress.percent + "%";
+  progressBarMessageElement.innerHTML = (progress.current / progress.total) * 100 + "%";
+}
+
+
+let bar = document.getElementById("progress-bar");
+let barMessage = document.getElementById("progress-bar-message");
+for (let i = 0; i < 11; i ++) {
+    setTimeout(updateProgress, 500 * i, bar, barMessage, {
+        percent: 10 * i,
+        current: 10 * i,
+        total: 100
+    })
+}
