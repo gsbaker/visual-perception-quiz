c7be9b5934e9608467e39e2830372e3a5c963a12
George Baker <gxb911@cs.bham.ac.uk>
2021-11-30 17:40:54 +0000

    Add custom manage.py command for creating questions manually

---
+++ mysite/quiz/management/commands/create_questions.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+from django.core.management.base import BaseCommand, CommandError
+from quiz.models import Question, Choice
+
+
+class Command(BaseCommand, ABC):
+    help = "Manually create questions"
+
+    def __init__(self):
+        super().__init__()
+        self.alpha_num_dict = {1: "A", 2: "B", 3: "C", 4: "D"}
+        self.question_text = "Which is the tallest line?"
+
+    def add_arguments(self, parser):
+        parser.add_argument('n', type=int)
+
+    def handle(self, *args, **options):
+        if options['n'] <= 0:
+            raise CommandError("Invalid argument ", options['n'])
+
+        for _ in range(options['n']):
+            question = Question()
+            question.question_text = self.question_text
+            question.save()
+
+            for i in range(1, 5):
+                choice = Choice()
+                choice.choice_text = "Choice {}".format(self.alpha_num_dict.get(i))
+                choice.question = question
+                choice.save()
