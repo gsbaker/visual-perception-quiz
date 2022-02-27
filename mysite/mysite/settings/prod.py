d2ac2474c8055069b2ca4408fb91d41f7150bfbc
Ubuntu <george@visual-perception-quiz.5peyg5l5g5cu5fjmsiyyuk10sf.bx.internal.cloudapp.net>
2022-02-27 19:33:23 +0000

    Update production settings

--- mysite/mysite/settings/prod.py
+++ mysite/mysite/settings/prod.py
@@ -1,5 +1,21 @@
 from mysite.settings.base import *
+import os
 
 # Override base settings here
 
 DEBUG = False
+
+SECRET_KEY = os.environ['SECRET_KEY'] 
+
+ALLOWED_HOSTS = ['40.71.223.198', 'www.visualperceptionquiz.com', 'https://www.visualperceptionquiz.com', 'visualperceptionquiz.com', 'localhost']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'quiz',
+        'USER': 'george',
+        'PASSWORD': 'django1234',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
