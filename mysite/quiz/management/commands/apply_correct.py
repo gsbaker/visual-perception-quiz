194a8035c173b7f6c6f044a0e7c0ee813632b49f
George Baker <gxb911@cs.bham.ac.uk>
2022-03-21 17:36:45 +0000

    Add apply_correct and update results command

---
+++ mysite/quiz/management/commands/apply_correct.py
@@ -0,0 +1,52 @@
+from abc import ABC
+
+from django.core.management.base import BaseCommand, CommandError
+from quiz.models import Question, Choice
+
+import csv
+from django.contrib.staticfiles.storage import staticfiles_storage
+
+
+class Command(BaseCommand, ABC):
+    help = "Counts the number of questions"
+
+    def __init__(self):
+        super().__init__()
+        self.answer_dict = {4: "B", 5: "C", 6: "A", 7: "D", 8: "C", 9: "B", 10: "C", 11: "D", 12: "B", 13: "A",
+                            14: "D", 15: "C", 16: "B", 17: "C", 18: "D", 19: "D", 20: "D"}
+
+    def handle(self, *args, **options):
+        questions = Question.objects.all()
+        for question in questions:
+            is_clear_answer = question.image_number not in [1, 2, 3, 21, 22, 23]
+            if is_clear_answer:
+                choices = question.choice_set.all()
+                correct_answer = self.answer_dict[question.image_number]
+                for choice in choices:
+                    if choice.choice_text == correct_answer:
+                        choice.correct = True
+                    else:
+                        choice.correct = False
+                    choice.save()
+            else:
+                if question.image_number == 21:
+                    choices = question.choice_set.all()
+                    for choice in choices:
+                        if choice.choice_text in ["B", "D"]:
+                            choice.correct = True
+                        else:
+                            choice.correct = False
+                        choice.save()
+                elif question.image_number == 22:
+                    choices = question.choice_set.all()
+                    for choice in choices:
+                        if choice.choice_text in ["A", "B", "C"]:
+                            choice.correct = True
+                        else:
+                            choice.correct = False
+                        choice.save()
+                elif question.image_number == 23:
+                    choices = question.choice_set.all()
+                    for choice in choices:
+                        choice.correct = True
+                        choice.save()
--- mysite/quiz/management/commands/results.py
+++ mysite/quiz/management/commands/results.py
@@ -1,7 +1,7 @@
 from abc import ABC
 
 from django.core.management.base import BaseCommand, CommandError
-from quiz.models import User
+from quiz.models import User, Question
 
 
 class Command(BaseCommand, ABC):
@@ -9,52 +9,56 @@ class Command(BaseCommand, ABC):
 
     def __init__(self):
         super().__init__()
+        self.users = User.objects.all()
+        self.set_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
     def handle(self, *args, **options):
-        users = User.objects.all()
-        answers_51 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-        }
+        self.same_length_answers()
+        self.collate_correct_responses()
+        print(self.set_dict)
 
-        answers_52 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-        }
+    def collate_correct_responses_helper(self, question_id, user_choice, set_number):
+        question = Question.objects.get(pk=question_id)
+        choices = question.choice_set.all()
+        for choice in choices:
+            if choice.correct:
+                if user_choice == choice.choice_text:
+                    self.set_dict[set_number] += 1
 
-        answers_53 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-        }
+    def collate_correct_responses(self):
+        for user in self.users:
+            answers = user.answers
+            if len(answers) > 0:
+                for answer in answers:
+                    pair = answer.split(",")
+                    q_id = int(pair[0])
+                    user_choice = pair[1]
+                    if q_id in range(4, 14):
+                        self.collate_correct_responses_helper(q_id, user_choice, 1)
+                    elif q_id in range(14, 24):
+                        self.collate_correct_responses_helper(q_id, user_choice, 2)
+                    elif q_id in range(24, 34):
+                        self.collate_correct_responses_helper(q_id, user_choice, 3)
+                    elif q_id in range(34, 44):
+                        self.collate_correct_responses_helper(q_id, user_choice, 4)
+                    elif q_id in range(44, 54):
+                        self.collate_correct_responses_helper(q_id, user_choice, 5)
+                    elif q_id in range(54, 64):
+                        self.collate_correct_responses_helper(q_id, user_choice, 6)
+                    elif q_id in range(64, 74):
+                        self.collate_correct_responses_helper(q_id, user_choice, 7)
+                    elif q_id in range(74, 84):
+                        self.collate_correct_responses_helper(q_id, user_choice, 8)
 
-        answers_61 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-         }
-
-        answers_62 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-         }
-
-        answers_63 = {
-            "A": 0,
-            "B": 0,
-            "C": 0,
-            "D": 0,
-         }
-
-        for user in users:
+    def same_length_answers(self):
+        answers_dict = {"A": 0, "B": 0, "C": 0, "D": 0}
+        answers_51 = answers_dict.copy()
+        answers_52 = answers_dict.copy()
+        answers_53 = answers_dict.copy()
+        answers_61 = answers_dict.copy()
+        answers_62 = answers_dict.copy()
+        answers_63 = answers_dict.copy()
+        for user in self.users:
             answers = user.answers
             if len(answers) > 0:
                 for answer in answers:
@@ -72,11 +76,9 @@ class Command(BaseCommand, ABC):
                         answers_62[pair[1]] += 1
                     elif q_id == 63:
                         answers_63[pair[1]] += 1
-
         print(51, answers_51)
         print(52, answers_52)
         print(53, answers_53)
         print(61, answers_61)
         print(62, answers_62)
         print(63, answers_63)
-
