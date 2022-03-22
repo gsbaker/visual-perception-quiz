643a90eda4606d5f2e997425f9afaf359d0f6ea6
George Baker <gxb911@cs.bham.ac.uk>
2022-03-22 22:15:31 +0000

    Update results command

--- mysite/quiz/management/commands/results.py
+++ mysite/quiz/management/commands/results.py
@@ -13,9 +13,17 @@ class Command(BaseCommand, ABC):
         self.set_dict = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
 
     def handle(self, *args, **options):
-        self.same_length_answers()
+        print("Full Data Sets:", self.count_full_data_sets())
+        print("Q51 Results:", self.inspect_answers(51))
         self.collate_correct_responses()
         print(self.set_dict)
+
+    def count_full_data_sets(self):
+        count = 0
+        for user in self.users:
+            if len(user.used_question_ids) == 83:
+                count += 1
+        return count
 
     def collate_correct_responses_helper(self, question_id, user_choice, set_number):
         question = Question.objects.get(pk=question_id)
@@ -50,35 +58,14 @@ class Command(BaseCommand, ABC):
                     elif q_id in range(74, 84):
                         self.collate_correct_responses_helper(q_id, user_choice, 8)
 
-    def same_length_answers(self):
+    def inspect_answers(self, question_id):
         answers_dict = {"A": 0, "B": 0, "C": 0, "D": 0}
-        answers_51 = answers_dict.copy()
-        answers_52 = answers_dict.copy()
-        answers_53 = answers_dict.copy()
-        answers_61 = answers_dict.copy()
-        answers_62 = answers_dict.copy()
-        answers_63 = answers_dict.copy()
         for user in self.users:
             answers = user.answers
             if len(answers) > 0:
                 for answer in answers:
                     pair = answer.split(",")
-                    q_id = int(pair[0])
-                    if q_id == 51:
-                        answers_51[pair[1]] += 1
-                    elif q_id == 52:
-                        answers_52[pair[1]] += 1
-                    elif q_id == 53:
-                        answers_53[pair[1]] += 1
-                    elif q_id == 61:
-                        answers_61[pair[1]] += 1
-                    elif q_id == 62:
-                        answers_62[pair[1]] += 1
-                    elif q_id == 63:
-                        answers_63[pair[1]] += 1
-        print(51, answers_51)
-        print(52, answers_52)
-        print(53, answers_53)
-        print(61, answers_61)
-        print(62, answers_62)
-        print(63, answers_63)
+                    current_question_id = int(pair[0])
+                    if current_question_id == question_id:
+                        answers_dict[pair[1]] += 1
+        return answers_dict
